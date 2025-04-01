b71a54c9bd46259a561b3dfd56c7c79cab34589e
Jovan <jovan.dimovski@codeland.it>
2025-04-01 18:35:41 +0200

    multiple carousel fixes

--- ui.apps/src/main/content/jcr_root/apps/wknd/components/customcarousel/clientlibs/site/js/carousel.js
+++ ui.apps/src/main/content/jcr_root/apps/wknd/components/customcarousel/clientlibs/site/js/carousel.js
@@ -4,13 +4,10 @@ function initImageCarousel() {
     var imageCarousels = document.querySelectorAll(".swiper-container");
     imageCarousels = Array.from(imageCarousels).filter(el => el.id.includes("image-carousel"));
 
-
     imageCarousels.forEach(function (imageCarouselElement) {
         var id = imageCarouselElement.id;
-        console.log("Initializing Image Carousel with id: " + id);
 
         if (id) {
-            // Extract parameters from the HTML attributes
             var spaceBetween = parseInt(imageCarouselElement.getAttribute("space-between")) || 0;
             var navigation = imageCarouselElement.getAttribute("navigation") === "true";
             var keyboard = imageCarouselElement.getAttribute("keyboard") === "true";
@@ -21,7 +18,6 @@ function initImageCarousel() {
 
             console.log("Image Carousel " + id + " initialized with initSlide: " + initSlide);
 
-            // Initialize the Swiper for this carousel
             new Swiper(`#${id} .swiper`, {
                 breakpoints: {
                     "@0.00": { // Mobile view
@@ -57,96 +53,107 @@ function initImageCarousel() {
 }
 
 function initThumbCarousel() {
-    // Select the carousel element
-    var thumbCarouselElement = document.getElementById("thumb-carousel");
+    var thumbCarousels = document.querySelectorAll(".feature-carousel");
 
-    if (thumbCarouselElement) {
-        // Extract parameters from the HTML attributes
-        var spaceBetween = parseInt(thumbCarouselElement.getAttribute("space-between")) || 0;
-        var navigation = thumbCarouselElement.getAttribute("navigation") === "true";
-        var keyboard = thumbCarouselElement.getAttribute("keyboard") === "true";
-        var pagination = thumbCarouselElement.getAttribute("pagination") === "true";
-        var paginationClickable = thumbCarouselElement.getAttribute("pagination-clickable") === "true";
+    thumbCarousels.forEach(function (thumbCarouselElement) {
+        var id = thumbCarouselElement.id;
 
-        // Initialize the thumbnail Swiper
-        var swiperThumbs = new Swiper(".mySwiper", {
-            spaceBetween: spaceBetween,
-            slidesPerView: 4,
-            freeMode: true,
-            watchSlidesProgress: true,
-        });
+        if (id) {
+            var spaceBetween = parseInt(thumbCarouselElement.getAttribute("space-between")) || 0;
+            var navigation = thumbCarouselElement.getAttribute("navigation") === "true";
+            var keyboard = thumbCarouselElement.getAttribute("keyboard") === "true";
+            var pagination = thumbCarouselElement.getAttribute("pagination") === "true";
+            var paginationClickable = thumbCarouselElement.getAttribute("pagination-clickable") === "true";
 
-        // Initialize the main Swiper
-        var swiperMain = new Swiper(".mySwiper2", {
-            spaceBetween: spaceBetween,
-            loop: true,
-            navigation: navigation
-                ? {
-                      nextEl: ".swiper-button-next",
-                      prevEl: ".swiper-button-prev",
-                  }
-                : false,
-            keyboard: keyboard ? { enabled: true } : false,
-            pagination: pagination
-                ? {
-                      el: ".swiper-pagination",
-                      clickable: paginationClickable,
-                  }
-                : false,
-            thumbs: {
-                swiper: swiperThumbs,
-            },
-        });
-    }
+            // Initialize the thumbnail Swiper
+            var swiperThumbs = new Swiper(`#${id} .mySwiper`, {
+                spaceBetween: spaceBetween,
+                slidesPerView: 4,
+                freeMode: true,
+                watchSlidesProgress: true,
+            });
+
+            // Initialize the main Swiper
+            var swiperMain = new Swiper(`#${id} .mySwiper2`, {
+                spaceBetween: spaceBetween,
+                loop: true,
+                navigation: navigation
+                    ? {
+                        nextEl: `#${id} .swiper-button-next`,
+                        prevEl: `#${id} .swiper-button-prev`,
+                    }
+                    : false,
+                keyboard: keyboard ? { enabled: true } : false,
+                pagination: pagination
+                    ? {
+                        el: `#${id} .swiper-pagination`,
+                        clickable: paginationClickable,
+                    }
+                    : false,
+                thumbs: {
+                    swiper: swiperThumbs,
+                },
+            });
+        }
+    });
 }
 
 function initResponsiveCarousel() {
-    // Select the carousel element
-    var responsiveCarouselElement = document.getElementById("responsive-carousel");
-    if (responsiveCarouselElement) {
-        // Extract parameters from the HTML attributes
-        var spaceBetween = parseInt(responsiveCarouselElement.getAttribute("space-between")) || 0;
-        var navigation = responsiveCarouselElement.getAttribute("navigation") === "true";
-        var keyboard = responsiveCarouselElement.getAttribute("keyboard") === "true";
-        var pagination = responsiveCarouselElement.getAttribute("pagination") === "true";
-        var paginationClickable = responsiveCarouselElement.getAttribute("pagination-clickable") === "true";
-        var slidesPerView = parseInt(responsiveCarouselElement.getAttribute("slides-per-view")) || 1;
-        var initSlide = parseInt(responsiveCarouselElement.getAttribute("initial-slide")) || 1;
+    var responsiveCarousels = document.querySelectorAll(".swiper-container");
+    responsiveCarousels = Array.from(responsiveCarousels).filter(el => el.id.includes("responsive-carousel"));
 
-        // Initialize the Swiper
-        var swiperMain = new Swiper(".responsive-carousel", {
-            breakpoints: {
-                "@0.00": { // Mobile view
-                    slidesPerView: 1.2, // Show part of the next slide
+    responsiveCarousels.forEach(function (responsiveCarouselElement) {
+        var id = responsiveCarouselElement.id;
+
+        if (id) {
+            // Extract parameters from the HTML attributes
+            var spaceBetween = parseInt(responsiveCarouselElement.getAttribute("space-between")) || 0;
+            var navigation = responsiveCarouselElement.getAttribute("navigation") === "true";
+            var keyboard = responsiveCarouselElement.getAttribute("keyboard") === "true";
+            var pagination = responsiveCarouselElement.getAttribute("pagination") === "true";
+            var paginationClickable = responsiveCarouselElement.getAttribute("pagination-clickable") === "true";
+            var slidesPerView = parseInt(responsiveCarouselElement.getAttribute("slides-per-view")) || 1;
+            var initSlide = parseInt(responsiveCarouselElement.getAttribute("initial-slide")) || 1;
+
+            console.log("Responsive Carousel " + id + " initialized with initSlide: " + initSlide);
+
+            // Initialize the Swiper for this carousel
+            new Swiper(`#${id} .swiper`, {
+                breakpoints: {
+                    "@0.00": { // Mobile view
+                        slidesPerView: 1.2, // Show part of the next slide
+                    },
+                    768: { // Tablet view
+                        slidesPerView: 2, // Show exactly 2 slides
+                    },
+                    1024: { // Desktop screens
+                        slidesPerView: slidesPerView, // Maintain partial slide view
+                        spaceBetween: 20,
+                    }
                 },
-                768: { // Tablet view
-                    slidesPerView: 2, // Show exactly 2 slides
+                loop: true,
+                initialSlide: initSlide,
+                spaceBetween: spaceBetween,
+                centeredSlides: true,
+                hashNavigation: {
+                    watchState: navigation,
                 },
-                1024: { // Desktop screens
-                    slidesPerView: slidesPerView, // Maintain partial slide view
-                    spaceBetween: 20, 
-                }
-            },
-            loop: true,
-            initialSlide: initSlide,
-            spaceBetween: spaceBetween,
-            centeredSlides: true,
-            hashNavigation: {
-                watchState: navigation,
-            },
-            keyboard: keyboard ? { enabled: true } : false,
-            pagination: pagination
-                ? {
-                      el: ".swiper-pagination",
-                      clickable: paginationClickable,
-                  }
-                : false,
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-        });
-    }
+                keyboard: keyboard ? { enabled: true } : false,
+                pagination: pagination
+                    ? {
+                        el: `#${id} .swiper-pagination`,
+                        clickable: paginationClickable,
+                    }
+                    : false,
+                navigation: navigation
+                    ? {
+                        nextEl: `#${id} .swiper-button-next`,
+                        prevEl: `#${id} .swiper-button-prev`,
+                    }
+                    : false,
+            });
+        }
+    });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
