d8011e80f747ccb692afbe9e321f5868ba57ef19
Jovan <jovan.dimovski@codeland.it>
2025-04-28 17:30:15 +0200

    anchor link component

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/AnchorLink.java
@@ -0,0 +1,21 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+import lombok.Getter;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Getter
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class AnchorLink {
+
+    @ValueMapValue
+    private String title;
+
+    @ValueMapValue
+    private String linksTo;
+
+    @ValueMapValue
+    private String altText;
+}
---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/AnchorLinksModel.java
@@ -0,0 +1,48 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+import lombok.Getter;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ChildResource;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Model(adaptables = SlingHttpServletRequest.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class AnchorLinksModel {
+
+    @Self
+    private SlingHttpServletRequest request;
+
+    @ValueMapValue
+    private String labels;
+
+    @ValueMapValue
+    private String ctaAltText;
+
+    @ValueMapValue
+    private String linksCTA;
+
+    @ValueMapValue
+    private boolean openInNewWindow;
+
+    @ValueMapValue
+    private boolean showCTA;
+
+    @ValueMapValue
+    private String componentId;
+
+    @ValueMapValue
+    private String titleStyles;
+
+    @ChildResource
+    private List<AnchorLink> anchorLinks;
+
+}
+
