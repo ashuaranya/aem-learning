0622d496674a8270e5abb0992a664ad8b4b29356
Jovan <jovan.dimovski@codeland.it>
2025-04-30 11:00:06 +0200

    desktop fixes added

--- ui.apps/src/main/content/jcr_root/apps/wknd/components/inpagenavigation/clientlibs/site/js/inpagenavigation.js
+++ ui.apps/src/main/content/jcr_root/apps/wknd/components/inpagenavigation/clientlibs/site/js/inpagenavigation.js
@@ -1,9 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
+
     document.querySelectorAll('.in-page-navigation-links').forEach(link => {
-        console.log("links first123")
         link.addEventListener('click', function () {
             document.querySelectorAll('.in-page-navigation-links').forEach(link => link.classList.remove('active'));
             this.classList.add('active');
         });
+    });
+
+    const dropdownTitle = document.querySelector('.dropdown-title');
+    const dropdownContainer = document.querySelector('.dropdown-container');
+
+    if (dropdownTitle && dropdownContainer) {
+        dropdownContainer.classList.remove('active');
+
+        dropdownTitle.addEventListener('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            dropdownContainer.classList.toggle('active');
+        });
+    }
+
+    document.addEventListener('click', function (e) {
+        if (!e.target.closest('.dropdown-container') && dropdownContainer) {
+            dropdownContainer.classList.remove('active');
+        }
     });
 });
